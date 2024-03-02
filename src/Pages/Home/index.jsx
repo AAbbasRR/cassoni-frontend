a95a848552eceb7e62362755ed34b85a90b42998
abbas rahimzadeh <arahimzadeh79@gmail.com>
2024-03-02 16:34:11 +0330

    feature: adding grid image section 2 on index

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,6 +2,7 @@ import PauseIcon from "@mui/icons-material/Pause";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import Footer from "Layouts/Footer";
 import Navbar from "Layouts/Navbar";
+import Grid from "@mui/material/Grid";
 import { useRef, useState, useEffect } from "react";
 import axios from "utils/axios";
 import style from "./style.module.scss";
@@ -11,6 +12,7 @@ const Home = () => {
 
   const [isPlaying, setIsPlaying] = useState(true);
   const [mainVideo, setMainVideo] = useState(null);
+  const [gridImages, setGridImage] = useState([]);
 
   useEffect(() => {
     getourStoryData();
@@ -24,6 +26,9 @@ const Home = () => {
   const getourStoryData = () => {
     axios.get("public/settings/video/list/").then((res) => {
       setMainVideo(res.data.results?.[0]);
+    });
+    axios.get("public/settings/image/list/").then((res) => {
+      setGridImage(res.data.results);
     });
   };
 
@@ -70,6 +75,18 @@ const Home = () => {
           <p>Loading...</p>
         )}
       </div>
+      <Grid container spacing={3} className={style.gridImage}>
+        {gridImages?.map((node, index) => (
+          <Grid className={style.gridImage__item} item xs={12} md={4} xl={3} key={index}>
+            <img
+              className={style.gridImageContainer__item__img}
+              src={node?.image}
+              alt={node?.alt}
+            />
+            <h6 className={`font_light ${style.gridImage__item__caption}`}>{node?.caption}</h6>
+          </Grid>
+        ))}
+      </Grid>
       <div className={style.emptyBody}></div>
       <Footer />
     </div>
